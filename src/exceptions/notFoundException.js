866d4dc5e385a76ab604fa27c55f8d6f91dc7c61
Odame <kwesi.danquah@daaseng.com>
2024-04-22 17:26:49 +0100

    Configured not found exception for unfound resources

--- src/exceptions/notFoundException.js
+++ src/exceptions/notFoundException.js
@@ -1,9 +1,11 @@
 import { StatusCodes } from "http-status-codes";
 
-export class NotFoundException extends Error {
+class NotFoundException extends Error {
   constructor(message = "Resource Not Found", status = StatusCodes.NOT_FOUND) {
     super(message);
     this.status = status;
     this.errorType = "NotFoundException";
   }
 }
+
+export default NotFoundException;
