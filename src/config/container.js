0022c6b6c9d09011bdaa4c13237145c89d7f4682
Odame <kwesi.danquah@daaseng.com>
2024-04-30 18:40:43 +0100

    Register RegisterService and RegisterController with Awilix
    Added RegisterService and RegisterController to the dependency injection container using Awilix.

--- src/config/container.js
+++ src/config/container.js
@@ -1,11 +1,17 @@
 const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
+const RegisterService = require("../services/registerService");
+const RegisterController = require("../controllers/registerController");
+
 // Initialize the container
 const container = createContainer();
 
 container.register({
   // Register classes with the container
   // exampleService: asClass(ExampleService).scoped(),
+
+  registerService: asClass(RegisterService).scoped(),
+  registerController: asClass(RegisterController).scoped(),
 });
 
 module.exports = container;
