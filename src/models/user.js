4bf4107e02e03c2f921a85be8208f82a43b51b9c
Odame <kwesi.danquah@daaseng.com>
2024-04-30 12:11:58 +0100

    update user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, ValidationError } = require("sequelize");
+const ValidationException = require("../exceptions/validationException");
+const logger = require("../config/logging");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
@@ -12,6 +14,31 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      dob: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          isOldEnough(value) {
+            const eighteenYearsAgo = new Date();
+            eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+
+            if (new Date(value) > eighteenYearsAgo) {
+              logger.error("Validation error. age mus be 18 or greater");
+              throw new ValidationException(
+                "You must be at least 18 years old to register"
+              );
+            }
+          },
+        },
       },
       username: {
         type: DataTypes.STRING,
@@ -28,6 +55,10 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       is_admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      is_suspended: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
       },
