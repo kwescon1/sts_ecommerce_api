16e2148c0764aac967c3d188cbbb5170c135d3a5
Odame <kwesi.danquah@daaseng.com>
2024-05-01 22:45:36 +0100

    Implement generateToken function for JWT creation

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,5 +1,7 @@
 // utils.js
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 /**
  * Hashes a password using bcrypt with a predefined salt rounds.
@@ -11,6 +13,22 @@ const hashPassword = (password) => {
   return bcrypt.hashSync(password, saltRounds);
 };
 
+const generateToken = (user) => {
+  const secret = config.jwt.secret;
+
+  const expiresIn = config.jwt.expiry;
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+    isAdmin: user.is_admin,
+    isSuspended: user.is_suspended,
+  };
+
+  return jwt.sign(payload, secret, { expiresIn });
+};
+
 module.exports = {
   hashPassword,
+  generateToken,
 };
