f449c462e4df398921a814e141bc227b66674508
Odame <kwesi.danquah@daaseng.com>
2024-05-15 10:56:36 +0100

    setup worker for application

---
+++ src/config/worker.js
@@ -0,0 +1,22 @@
+// workerConfig.js
+const config = require("./config");
+
+const redisConfig = {
+  redis: {
+    host: config.redis.host,
+    port: config.redis.internal,
+  },
+};
+
+const defaultQueue = "default_queue"; // Default queue name
+const registeredUserQueue = "registered_users";
+const deletedUserQueue = "deleted_users";
+const suspendeUserQueue = "suspended_users";
+
+module.exports = {
+  defaultQueue,
+  registeredUserQueue,
+  deletedUserQueue,
+  suspendeUserQueue,
+  redisConfig,
+};
