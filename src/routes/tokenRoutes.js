52adaa5372e0fbc22ffb69c811b8509fd0dd06d8
Odame <kwesi.danquah@daaseng.com>
2024-05-16 21:11:57 +0100

    updated routes with swagger documentation

--- src/routes/tokenRoutes.js
+++ src/routes/tokenRoutes.js
@@ -13,6 +13,84 @@ tokenRoutes.use((req, res, next) => {
   next();
 });
 
+/**
+ * @swagger
+ * tags:
+ *   name: Token Management
+ *   description: Endpoints for managing access and refresh tokens
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/refresh-token:
+ *   post:
+ *     summary: Refreshes an expired access token using a valid refresh token.
+ *     tags: [Token Management]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refresh_token:
+ *                 type: string
+ *                 description: A string representing the refresh token.
+ *                 example: "<refresh_token>"
+ *     responses:
+ *       200:
+ *         description: Token generated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     accessToken:
+ *                       type: string
+ *                     refreshToken:
+ *                       type: string
+ *                 message:
+ *                   type: string
+ *             example:
+ *               success: true
+ *               data:
+ *                 accessToken: "<new_access_token>"
+ *                 refreshToken: "<new_refresh_token>"
+ *               message: "Token generated successfully."
+ *       400:
+ *         description: Invalid refresh token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 error:
+ *                   type: string
+ *             example:
+ *               success: false
+ *               error: "Invalid refresh token"
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 error:
+ *                   type: string
+ *             example:
+ *               success: false
+ *               error: "Server error"
+ */
 tokenRoutes.post(
   "/refresh-token",
   tokenValidationRules(),
