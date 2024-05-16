a8fb7632444f4b90d3ed13db7f7a470d0d2725b7
Odame <kwesi.danquah@daaseng.com>
2024-05-16 21:10:10 +0100

    updated seeder file

--- src/seeders/20240515100655-seed-products-and-stocks.js
+++ src/seeders/20240515100655-seed-products-and-stocks.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const { v4: uuidv4 } = require("uuid");
+const { generate } = require("../utilities/skuGenerator");
+const redisService = require("../services/redisService");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -25,11 +27,27 @@ module.exports = {
 
     for (let i = 1; i <= 50; i++) {
       const id = uuidv4();
+      const category_id = getRandomElement(categories);
+
+      // Get the latest SKU number from Redis or initialize it
+      const latestSkuNumber =
+        (await redisService.get("latest_sku")) || i.toString().padStart(4, "0");
+      const sku = generate(category_id, latestSkuNumber);
+
+      console.log(sku.split("-")[2]);
+
+      // Update the latest SKU number in Redis
+      await redisService.set(
+        "latest_sku",
+        sku.split("-")[2], // Extract the unique SKU number
+        60 * 60 // Cache for 1 hour
+      );
+
       products.push({
         id,
         name: `Product ${i}`,
-        category_id: getRandomElement(categories),
-        sku: `SKU${i.toString().padStart(4, "0")}`,
+        category_id,
+        sku,
         last_updated_restock_level: Math.floor(Math.random() * 100),
         current_stock_level: Math.floor(Math.random() * 100),
         created_at: new Date(),
