dff7d792a9a8af40da44040da600f8e1e2bb378d
Odame <kwesi.danquah@daaseng.com>
2024-05-24 03:18:12 +0100

    added validation rules for processing user cart

--- src/requests/cartRequest.js
+++ src/requests/cartRequest.js
@@ -45,7 +45,58 @@ const updateCartValidationRules = () => [
     .withMessage("Quantity must be a positive integer"),
 ];
 
+const processCartValidationRules = () => [
+  param("cart_id")
+    .notEmpty()
+    .withMessage("Cart ID is required")
+    .bail()
+    .isUUID()
+    .withMessage("Cart ID must be a valid UUID")
+    .bail()
+    .custom(async (value) => {
+      const cart = await Cart.findByPk(value);
+      if (!cart) {
+        throw new ValidationException("Cart not found");
+      }
+      return true;
+    }),
+  // Add validation for billing_is_shipping and shipping address
+  body("billing_is_shipping")
+    .isBoolean()
+    .withMessage("Billing is shipping is required and must be a boolean value"),
+  body("street_address")
+    .if(body("billing_is_shipping").equals(false))
+    .notEmpty()
+    .withMessage("Street address is required")
+    .isString()
+    .withMessage("Street address must be a string"),
+  body("city")
+    .if(body("billing_is_shipping").equals(false))
+    .notEmpty()
+    .withMessage("City is required")
+    .isString()
+    .withMessage("City must be a string"),
+  body("state")
+    .if(body("billing_is_shipping").equals(false))
+    .optional()
+    .isString()
+    .withMessage("State must be a string"),
+  body("postal_code")
+    .if(body("billing_is_shipping").equals(false))
+    .notEmpty()
+    .withMessage("Postal code is required")
+    .isString()
+    .withMessage("Postal code must be a string"),
+  body("country")
+    .if(body("billing_is_shipping").equals(false))
+    .notEmpty()
+    .withMessage("Country is required")
+    .isString()
+    .withMessage("Country must be a string"),
+];
+
 module.exports = {
   storeCartValidationRules,
   updateCartValidationRules,
+  processCartValidationRules,
 };
