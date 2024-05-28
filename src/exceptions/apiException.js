263773b308e9d0d574a00588ea4a1d2a8453bcc0
Odame <kwesi.danquah@daaseng.com>
2024-05-28 21:24:18 +0100

    exception

--- src/exceptions/apiException.js
+++ src/exceptions/apiException.js
@@ -1,7 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 
 class ApiException extends Error {
-  constructor(message = "Api Error Occured", status) {
+  constructor(
+    message = "Api Error Occured",
+    status = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
     super(message);
     this.status = status;
     this.errorType = "ApiException";
